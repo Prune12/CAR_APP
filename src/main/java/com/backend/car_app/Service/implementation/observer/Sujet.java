4520674f2cdccd0bded8ca1859ad7f9bf94a3334
Tomdieu <ivantomdio@gmail.com>
2024-02-08 13:51:32 +0100

    feat : remove observer

--- src/main/java/com/backend/car_app/Service/implementation/observer/Sujet.java
+++
@@ -1,8 +0,0 @@
-package com.backend.car_app.Service.implementation.observer;
-
-public abstract class Sujet {
-    private Observateur observateur;
-    public abstract void ajouterObservateur(Observateur o);
-    public abstract void supprimerObservateur(Observateur o);
-    public abstract void notifierObservateurs();
-}
